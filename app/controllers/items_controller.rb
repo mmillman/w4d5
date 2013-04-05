541efd4aa713020d4b231220aecfe0eaf1c09008
Mark Millman <markmillman@gmail.com>
2013-04-05 11:17:36 -0700

    Add actions for projects controller and items controller.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,28 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def create
+    @item = Item.new(params[:items])
+    @item.save
+
+    redirect_to item_url(@item)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def show
+    @item = Item.find(params[:id])
+
+    redirect_to item_url(@item)
+  end
+
+  def destroy
+    @item = Item.find(params[:id]).destroy
+
+    redirect_to items_url
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,28 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def create
+    @project = Project.new(params[:project])
+    @project.save
+
+    redirect_to project_url(@project)
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def show
+    @project = Project.find(params[:id])
+
+    redirect_to project_url(@project)
+  end
+
+  def destroy
+    @project = Project.find(params[:id]).destroy
+
+    redirect_to projects_url
+  end
 end
